90b36456a1c6920f9cc8a1c712a29743768231b3
Nurali <98206848+NuraliYuldashov@users.noreply.github.com>
2024-04-02 17:30:18 +0500

    katta o'zgarishdan oldin push

---
+++ MVCLayer/Controllers/ProductContorller.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces; // Assuming your LaptopDto class is in this namespace
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCLayer.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ILaptopService _laptopService;
+
+        public ProductController(ILaptopService laptopService)
+        {
+            _laptopService = laptopService;
+        }
+
+        public async Task<IActionResult> Laptop(int id)
+        {
+            var laptop = await _laptopService.GetByIdAsync(id);
+            return View(laptop);
+        }
+    }
+}
